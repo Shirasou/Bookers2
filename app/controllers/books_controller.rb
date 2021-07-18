458a060614d55d989e3304fae343660153401613
EC2 Default User <ec2-user@ip-172-31-33-237.ec2.internal>
2021-07-18 16:38:15 +0000

    save

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,13 +9,34 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @book = Book.new
+    @user = current_user
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to bookspath
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@user.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
   root to: 'homes#top'
+  get 'homes/about'
   resources :books, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update, :index]
 
 end
