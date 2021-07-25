e0fb721e3711875f299021a646718f803c86524d
EC2 Default User <ec2-user@ip-172-31-33-237.ec2.internal>
2021-07-25 16:02:23 +0000

    Save

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_20_053850) do
+ActiveRecord::Schema.define(version: 2021_07_25_142835) do
+
+  create_table "book_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.text "title"
     t.string "book_id"
     t.text "body"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
