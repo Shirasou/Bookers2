15cd34efd2ec15fd4286c1d439e31d991b7ea9f8
EC2 Default User <ec2-user@ip-172-31-33-237.ec2.internal>
2021-08-05 07:14:31 +0000

    finish-js

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,29 +1,27 @@
 class BookCommentsController < ApplicationController
+	before_action :authenticate_user!
 
-  def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    if comment.save
-    redirect_to book_path(book)
-    else
-      @error_comment = comment
-      @book = Book.find(params[:book_id])
-      @book_new = Book.new
-      @user = @book.user
-      @book_comment = BookComment.new
-      render 'books/show'
-    end
-  end
+	def create
+		@book = Book.find(params[:book_id])
+		@book_comment = BookComment.new(book_comment_params)
+		@book_comment.book_id = @book.id
+		@book_comment.user_id = current_user.id
+		if @book_comment.save
+			render :create
+		else
+		  render 'books/show'
+		end
+	end
 
-  def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
-  end
+	def destroy
+		@book = Book.find(params[:book_id])
+  		@book_comment = @book.book_comments.find(params[:id])
+		@book_comment.destroy
+		render :destroy
+	end
 
-  private
-  def book_comment_params
-    params.require(:book_comment).permit(:comment)
-  end
-
+	private
+	def book_comment_params
+		params.require(:book_comment).permit(:comment, :book_id, :user_id)
+	end
 end
